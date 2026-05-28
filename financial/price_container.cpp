0e5d96d6083c68092bbb47eb72706fb6811ee729
PraxedisJRuv <praxjruv@gmail.com>
2026-05-27 19:00:31 -0600

    add:Monte Carlo Euorpean payoff method and black Scholes for pricining

--- financial/price_container.cpp
+++ financial/price_container.cpp
@@ -43,6 +43,11 @@ class PriceContainer{
         std::cout<<"\n";
     }
 
+    double current_price(){
+        int n=close.size();
+        return close[n-1];
+    }
+
     PriceContainer returns(){
         std::vector<double> vreturns(close.size()-1);
         for (int i=0; i<close.size()-1; i++){
@@ -52,11 +57,14 @@ class PriceContainer{
     }
 };
 
+double normalCDF(double x){
+    return 0.5*std::erfc(-x/std::sqrt(2.0));
+}
 
 std::vector<PriceContainer> simulateBGM(PriceContainer& s, double mu, double vola, int T, double M, double n){
     std::vector<double> vec(n+1,0);
     std::vector<PriceContainer> paths(M,PriceContainer(vec));
-    std::mt19937 rng(777);
+    std::mt19937 rng(std::random_device{}());
     std::normal_distribution<double> Z(0,1);
     double delta=T/n;
     double multiplier=0;
@@ -64,28 +72,60 @@ std::vector<PriceContainer> simulateBGM(PriceContainer& s, double mu, double vol
 
     for (double i=0; i<M; i++){
         paths[i].close[0]=s.close[last-1];
-        std::cout<<paths[i].close[0]<<"\t";
+        //std::cout<<paths[i].close[0]<<"\t";
     }
-    std::cout<<"\n";
+    //std::cout<<"\n";
 
     for (double j=1; j<n; j++){
 
         for (double i=1; i<M; i++){
             multiplier =std::exp(mu*delta+vola*sqrt(delta)*Z(rng));
             paths[i].close[j]=((paths[i].close[j-1])*multiplier);
-            std::cout<<paths[i].close[j]<<"\t";
+            //std::cout<<paths[i].close[j]<<"\t";
         }
-        std::cout<<"\n";
+        //std::cout<<"\n";
     }
 
     return paths;
 }
 
+/*
+mu is the expected rate of return. Percentage growth
+vola is the volatility of the asset The standard deviation of the asset's continuously compounded (log) returns
+T is the time horizon, generally is a year
+M is the amount of paths
+n is the amoun of steps in the simulation, that's why our "delta" is T/n
+k is the strike or a fixed price for a stock with expiration T
+r is the return risk-free rate
+the exp(-r*T) is a discount thinking that x money tomorrow worths less than x money today
+We're getting the current payoff for something sold at T
+*/
 
-std::mt19937 rng(777);
+double mc_european_call_payoff(PriceContainer stock, double mu, double vola, int T, double M, double n, double k, double r){
+    std::vector<PriceContainer> paths=simulateBGM(stock, mu, vola, T, M, n);
+    double sum=0;
+    for (int i=0; i<M; i++){
+        sum=sum+std::max(paths[i].close[n-1]-k,0.0);
+    }
+    double payoff_mean=(std::exp(-r*T))*(sum/M);
+    return payoff_mean;
+}
+
+double black_scholes(PriceContainer stock, double vola, int T, double k, double r){
+    double d1= (std::log(stock.current_price()/k)+(r+0.5*vola*vola)*T)/(vola*std::sqrt(T));
+    double d2=d1-(vola*std::sqrt(T));
+
+    return stock.current_price()*normalCDF(d1)-k*(std::exp(-r*T))*normalCDF(d2);
+
+}
 
 int main(){
     PriceContainer test({10,10});
-    std::vector<PriceContainer> paths=simulateBGM(test,0.1,0.3,1,15,100);
+    //std::vector<PriceContainer> paths=simulateBGM(test,0.1,0.3,1,15,100);
+    double mc_price=mc_european_call_payoff(test,0.1,0.3,1,100000,252,11.0,0.1);
+    double bs_price=black_scholes(test,0.3,1,11.0,0.1);
+    double error =std::abs(mc_price -bs_price);
+    double errorpercent=error/bs_price;
+    std::cout<<mc_price<<"\t"<<bs_price<<"\t"<<error<<"\t"<<errorpercent<<"%";
     return 0;
 }
