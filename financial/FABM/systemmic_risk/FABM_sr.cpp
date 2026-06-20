9c22cf7e33fe247d600f1300e23926f915512323
PraxedisJRuv <praxjruv@gmail.com>
2026-06-19 23:24:01 -0600

    add: needed functions to propperly run FBAM risk simulation

--- financial/FABM/systemmic_risk/FABM_sr.cpp
+++ financial/FABM/systemmic_risk/FABM_sr.cpp
@@ -5,6 +5,14 @@
 
 enum class State{Solvent, Stressed, Defaulted};
 
+const double delta_S = 0.85;
+const double delta_D = 0.40;
+const double threshold_high = 0.08;
+const double threshold_low  = 0.03;
+
+const double p=1;
+int N;
+
 struct Banks{
     int id;
     double equity;
@@ -13,7 +21,7 @@ struct Banks{
     
     Banks(int id, State state){
         this->id=id;
-        current_state=state;
+        this->current_state=state;
 
         std::mt19937 rng(std::random_device{}());
         std::uniform_real_distribution<double>Z1(500,1500);
@@ -29,8 +37,8 @@ struct Banks{
 
     void classify(){
         double cr=capital_ratio();
-        if(cr<= threshold_low) current_state=State::Defaulted;
-        if(cr<=threshold_high) current_state=State::Stressed;
+        if(cr<= 0.03) current_state=State::Defaulted;
+        if(cr<=0.08) current_state=State::Stressed;
         else current_state=State::Solvent;
     }
 };
@@ -42,13 +50,19 @@ delta D the default recovery rate
 the treshold high would be from solvent to stressed
 the low would be from stressed to defaulted
 */
-const double delta_S = 0.85;
-const double delta_D = 0.40;
-const double threshold_high = 0.08;
-const double threshold_low  = 0.03;
 
-const double p=0.1;
-int N;
+
+int count_state(std::vector<Banks>& banks, State s){
+    int N=banks.size();
+    int count=0;
+    for (int i=0; i<N; i++){
+        if (s!=banks[i].current_state){
+            continue;
+        }
+        count++;
+    }
+    return count;
+}
 
 std::vector<std::vector<int>> conctacts_list(const int N, 
                                                 const double p, 
@@ -68,35 +82,91 @@ std::vector<std::vector<int>> conctacts_list(const int N,
 
 std::vector<std::vector<double>> exposure_random(const int N, const std::vector<std::vector<int>>& contacts){
     std::vector<std::vector<double>> exposure_matrix(N);
-    std::uniform_real_distribution<double>Z_debt(0,1500);
+    std::uniform_real_distribution<double>Z_debt(200000,5000000);
     for (int i=0; i<N; i++){
         int total_contacts=contacts[i].size();
-        for (int j=0; j<total_contacts; j++){
+        for (int j=0; j<N; j++){
             std::mt19937 rng(std::random_device{}());
             exposure_matrix[i].push_back(Z_debt(rng));
         }
     }
+    return exposure_matrix;
 }
 //define contagion function
 
 double compute_loss(int id, const std::vector<Banks>& banks,
-                    const std::vector<std::vector<double>>& exposure) {
+                     std::vector<std::vector<double>>& exposure, int const &N) {
     double loss = 0.0;
-    int N = banks.size();
-    for (int i = 0; i < N; i++) {
-        if (i == id) continue;
-        double owed = exposure[i][id]; // bank i owes bank id this amount
-        
-        if (owed <= 0.0) continue;
+    for (int i=0; i<N; i++){
+        int total_contacts=exposure[i].size();
+        for (int j=0; j<total_contacts; j++){
+            if (j!=id) continue;
+            
+            double owed = exposure[i][id]; // bank i owes bank id this amount
+            
+            if (owed <= 0.0) continue;
 
-        double recovery = 0.0;
-        switch (banks[i].current_state) {
-            case State::Solvent:   recovery = 1.0;     break;
-            case State::Stressed:  recovery = delta_S; break;
-            case State::Defaulted: recovery = delta_D; break;
+            double recovery = 0.0;
+            switch (banks[id].current_state) {
+                case State::Solvent:   recovery = 1.0;     break;
+                case State::Stressed:  recovery = delta_S; break;
+                case State::Defaulted: recovery = delta_D; break;
+            }
+            loss = loss+ (owed * (1.0 - recovery));
+            }
         }
-        loss = loss+ (owed * (1.0 - recovery));
-    }
     return loss;
 }
 
+void update_state(std::vector<Banks>& banks, std::vector<std::vector<double>>& exposure, const int& N){
+    for (int i=0; i<N; i++){
+        if (banks[i].current_state==State::Defaulted){
+            continue;
+        }
+        double loss = compute_loss(i, banks, exposure, N);
+        banks[i].equity=banks[i].equity-loss;
+        banks[i].classify();
+    }
+}
+
+int main(){
+    int Amount=50;
+    int Periods=10;
+
+    std::vector<Banks> banks;
+    for (int i=0; i<Amount-3; i++){
+        banks.push_back(Banks(i, State::Solvent));
+    }
+    for (int i=Amount-2; i<Amount; i++){
+        banks.push_back(Banks(i, State::Stressed));
+    }
+    banks.push_back(Banks(49,State::Defaulted));
+    banks[49].equity=0;
+
+
+    std::mt19937 rng(std::random_device{}());
+    std::vector<std::vector<int>> contacts=conctacts_list(Amount, p, rng);
+    std::vector<std::vector<double>> exposure=exposure_random(Amount, contacts);
+
+    for (int i=0; i<Amount; i++){
+        std::cout<<banks[i].equity<<" ";
+    }
+    std::cout<<"\n";
+
+    for (int j=0; j<Periods; j++){
+        std::cout<<"Period: "<<j<<"\t |"
+                 <<"Solvent: " <<count_state(banks,State::Solvent)<<"\t |"
+                 <<"Stressed: "<<count_state(banks,State::Stressed)<<"\t |"
+                 <<"Defaulted: "<<count_state(banks, State::Defaulted)<<"\n";
+        update_state(banks, exposure, Amount);
+    }
+
+     for (int i=0; i<Amount; i++){
+        std::cout<<banks[i].equity<<" ";
+    }
+    std::cout<<"\n";
+
+    std::cout<<banks[48].capital_ratio();
+    banks[48].classify();
+    return 0;
+}
